8d06f88ccf737dc6a5b5256b0cf6e712c6dcc7d8
GentaArya <mgentaarya@yahoo.com>
2023-05-22 20:07:57 +0700

    push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import React from "react";
+import MainLayouts from "./components/main.layouts";
+import "./service/api";
+import { getBerita } from "./service/api";
+import "./App.css";
+
 
 function App() {
+  const [news, setBerita] = useState([]);
+  useEffect(() => {
+    getBerita().then((ress) => {
+      setBerita(ress.articles);
+    });
+  }, []);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <MainLayouts>
+        <div className="container">
+          <h1>Breaking news</h1>
+          <div className="news-grid">
+            {news.map((article) => (
+              <div className="news-card" key={article.id}>
+                <img
+                  className="image"
+                  src={article.urlToImage}
+                  alt={article.title}
+                />
+                <div className="content">
+                  <h2 className="title">{article.title}</h2>
+                  <p className="description">{article.description}</p>
+                  <p className="published-at">{article.publishedAt}</p>
+                  <a
+                    className="read-more"
+                    href={article.url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Read more
+                  </a>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="footer">
+          <p>&copy; 2023 @DianaBloggers. All rights reserved.</p>
+        </div>
+      </MainLayouts>
+    </>
   );
 }
-
 export default App;
---
+++ src/service/api.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+// const api = "https://api.rajaongkir.com/starter/province?key=1feb04075e76d2f209f0a1142efc4d31"
+
+export const getBerita = () => {
+  return axios
+    .get(api)
+    .then((response) => response.data)
+    .catch((err) => {
+      throw err;
+    });
+};
+const api = "https://newsapi.org/v2/everything?q=tesla&from=2023-04-21&sortBy=publishedAt&apiKey=3c3fec3ba03344b4990ba080b3a0e99b";
